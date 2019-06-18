83439e78f41e3e8ae65d152244d0ade5d3ea46d7
Ken Akers <akers.kenneth66@gmail.com>
2019-06-18 14:53:05 -0700

    Add files via upload

--- test_script.ps1
+++ test_script.ps1
@@ -1,2 +1,12 @@
+# Go to the Desktop for easier accessibility
 cd C:\Users\Administrator\Desktop
+
+# Make a dummy directory to show it worked
 mkdir Test
+
+# Move into that directory
+cd Test
+
+# Create the SSH private key and store it here
+aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text | out-file -encod
+ing ascii -filepath private_key.pem
