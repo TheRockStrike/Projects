c47d8dcc921c5d38294eb11bcfca18f4bcafbfbd
Ken Akers <akers.kenneth66@gmail.com>
2019-07-02 15:02:25 -0700

    Update create-aws-vm.groovy

--- create-aws-vm.groovy
+++ create-aws-vm.groovy
@@ -188,7 +188,7 @@ node {
         string(name: 'KEY_PAIR_NAME', defaultValue: "", description: 'The name of the key pair created in AWS.')
         string(name: 'INSTANCE_NAME', defaultValue: "", description: 'The name of the instance that will be created. This will be the host name of the instance, so no spaces...')
         string(name: 'AMI', defaultValue: "", description: 'The Amazon Machine Image (AMI) ID used to specify the OS to run on the instance.')
-        boolean(name: 'DEBUG', defaultValue: false, description: 'Click to enable debugging in the console output.')
+        bool(name: 'DEBUG', defaultValue: false, description: 'Click to enable debugging in the console output.')
     }
     try {
         echo """
