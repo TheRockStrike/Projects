90e9659c9dfae4f1c986fd740538b18de68a1471
Ken Akers <akers.kenneth66@gmail.com>
2019-07-03 15:55:22 -0700

    Update create-aws-vm.groovy

--- create-aws-vm.groovy
+++ create-aws-vm.groovy
@@ -232,10 +232,9 @@ node {
         }
 
         stage('Attaching instance to Jenkins') { // In Progress
-            def command = "AWS-RunRemoteScript"
             def targets = "Key=instanceids,Values=${instanceID}"
             def parameters = '{"sourceType":["GitHub"],"sourceInfo":["{\"owner\": \"kennyakers\", \"repository\": \"Projects\", \"path\": \"ConnectNewVirtualMachineToJenkins.ps1\"}"],"commandLine":[".\\ConnectNewVirtualMachineToJenkins.ps1"],"workingDirectory":[""],"executionTimeout":["3600"]}'
-            def proc = "aws ssm send-command --document-name ${command} --document-version 1 --targets ${targets} --parameters ${parameters} --timeout-seconds 600 --max-concurrency 50 --max-errors 0 --region us-east-2".execute()
+            def proc = "aws ssm send-command --document-name AWS-RunRemoteScript --document-version 1 --targets ${targets} --parameters ${parameters} --timeout-seconds 600 --max-concurrency 50 --max-errors 0 --region us-east-2".execute()
 
             def sout = new StringBuilder(), serr = new StringBuilder()
             proc.consumeProcessOutput(sout, serr)
